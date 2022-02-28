f8ebe9b7ddb0440b24474ce819ef8dfe6a7d4b6f
Christopher <sadlerlogic@gmail.com>
2022-02-27 20:05:27 -0600

    commit tree 079025864b5908d0b3a6c2b3635ed4037000ca37

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate"){
+   return `This Saturday, I want to ${fun}!`;
+ 
+}
+
+const mondayWork = function(task = "go to the office"){
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective(result = "*"){
+  return function whateva(howeverYouWish = "special"){
+    return  `You are ${result}${howeverYouWish}${result}!`;
+
+}
+}
